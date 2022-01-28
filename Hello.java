146952407d63db8c14eefcea3751236f16d2b74f
helloAllyj <98562086+helloAllyj@users.noreply.github.com>
2022-01-27 21:27:28 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**********************************************************
+*Hello.java
+***********************************************************
+*Allyson Juarez
+*1/27/2022
+*255 C90
+***********************************************************/
+public class Hello {
+public static void main(String [] args){
+	System.out.println("Allyson Juarez");
+	System.out.println("Hello java");
+	System.out.println("Outputting information on command line");
+	System.out.println("1/27/2022");
+	System.out.println("255 C90");
+	System.out.println();
+	}
+	}
